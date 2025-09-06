d9d3ddb9d6f73cc9fc1f6a60c0739221ed28e67b
trixxyy-cloner <trixxarn@gmail.com>
2025-09-06 19:25:28 +0200

    Added index and navbar

--- Main/script.js
+++ Main/script.js
@@ -1,16 +1,26 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent actual form submission
+  e.preventDefault(); 
 
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
   const error = document.getElementById("error");
 
-  // Example for validation
+  
   if (username === "sonny" && password === "13371337") {
     alert("Login successful!");
-    // Redirect or proceed further
+    
     error.textContent = "";
   } else {
     error.textContent = "Invalid username or password.";
   }
 });
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
